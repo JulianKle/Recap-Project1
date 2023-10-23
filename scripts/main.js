64d6be0d29776e4a553494721d0a375cb3ddb034
JulianKle <148042845+JulianKle@users.noreply.github.com>
2023-10-23 14:26:09 +0000

    Change CSS Files

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,9 @@ const switchDark = document.querySelector('[data-js="toggle-button"]');
 switchDark.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
 });
+
+const bookmark = document.querySelector('[data-js="bookmark_click"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.add("dark");
+});

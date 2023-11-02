d2e4b999d6044116e643abb7b5eaf08ec8b64c4d
Julian Klett <julian22a@googlemail.com>
2023-11-02 10:24:34 +0100

    Change CSS/HTML/JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,11 @@
 const bodyElement = document.querySelector('[data-js="main"]');
 const switchDark = document.querySelector('[data-js="toggle-button"]');
+const bookmark = document.querySelector('[data-js="bookmark-click"]');
 
 switchDark.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
 });
 
-const bookmark = document.querySelector('[data-js="bookmark_click"]');
-
 bookmark.addEventListener("click", () => {
-  bookmark.classList.add("dark");
+  bodyElement.classList.toggle("red");
 });

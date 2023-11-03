ab09788aa6ea849ac21fc8c5483d39e2c5272867
Julian Klett <julian22a@googlemail.com>
2023-11-03 12:03:11 +0100

    add functionality for bookmark and showAnswer Button

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,24 @@
 const bodyElement = document.querySelector('[data-js="main"]');
 const switchDark = document.querySelector('[data-js="toggle-button"]');
 const bookmark = document.querySelector('[data-js="bookmark-click"]');
-
-switchDark.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
-});
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answerCard = document.querySelector('[data-js="answerCard"]');
 
 bookmark.addEventListener("click", () => {
-  bodyElement.classList.toggle("red");
+  if (bookmark.textContent === "🔖") {
+    bookmark.textContent = "Marked✔";
+  } else {
+    bookmark.textContent = "🔖";
+    console.log(bookmark.value);
+  }
+});
+
+answerButton.addEventListener("click", () => {
+  if (answerButton.textContent === "Show answer") {
+    answerButton.textContent = "Hide answer";
+    answerCard.hidden = false;
+  } else {
+    answerButton.textContent = "Show answer";
+    answerCard.hidden = true;
+  }
 });
---
+++ scripts/profile.js
@@ -0,0 +1,3 @@
+switchDark.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
